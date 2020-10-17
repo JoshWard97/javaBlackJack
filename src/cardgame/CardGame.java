4b4e122756ecb9cc5404c1ba3228ee0f2bd0c6c8
Joshwa97 <Joshwa97@gmail.com>
2020-10-17 00:37:29 -0500

    Lets user bet by hitting b until a c is entered, corectly calulates scoring, etc

--- src/cardgame/CardGame.java
+++ src/cardgame/CardGame.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -24,6 +25,9 @@ public class CardGame {
     public static String cardSymbol;
     public static String cardSign;
     public static Suit suitH;
+    public static int playerScore;
+    public static int dealerScore;
+    public static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
         // Create Deck
@@ -41,15 +45,115 @@ public class CardGame {
     }
      
     deck = shuffler(deck);
-    drawCard(deck[0],playerCards);
-    drawCard(deck[1],playerCards);
-    drawCard(deck[2],playerCards);
-    deck[3].hidden=true;
-    drawCard(deck[3],playerCards);
-    drawCard(deck[26],playerCards);
+    blackJack(deck,playerCards,dealerCards);
      
-    printCards(playerCards);
+    //printCards(playerCards);
      
+    }
+    public static void blackJack(card[] deck,String [] playerCards,String [] dealerCards)
+    {
+        while(true)
+        {
+            //Initial Drawing of Cards, player dealer, player, hidden dealer card. Starting at 1, not 0, to simulate discard.
+            int hitCountPlayer= 0;
+            int hitCountDealer;
+            int playerAces = 0; //aces player has. used to subtract to score for an ace being worth 1. Will display minimum max scores. (avoid confusion of showing 3 scores if 2 aces.)
+            drawCard(deck[1],playerCards);
+            drawCard(deck[2],dealerCards);
+            drawCard(deck[3],playerCards);
+            deck[4].hidden=true;
+            drawCard(deck[4],dealerCards);
+            blackJackPrintDecks(dealerCards,playerCards); //inital printing of deck after player and dealer have 2 cards
+            dealerScore = deck[2].value + deck[4].value;
+            playerScore = deck[1].value + deck[3].value;
+            if(playerScore==21) blackJackBlackJack();
+            if(deck[1].value==11)
+            {
+                playerAces++;
+                System.out.println("PLAYER HAS ACE");
+            }
+            if(deck[3].value==11)
+            {
+                System.out.println("PLAYER HAS ACE");
+                
+                playerAces++;
+            }
+            System.out.print("\nPlayer Score: " + playerScore);
+            int temp;
+            int tempAces = playerAces;
+            while(tempAces>0) //print another score for each ace.
+            {
+                temp = playerScore -= (10); //subtract 10 for each, show lowest possible score
+                System.out.print(" / " + temp);
+                tempAces--;            
+            }
+            
+            //at this point, each player has two cards.
+             //scanner for playing game
+            char c;
+            System.out.println("");
+            while('b'== sc.next().charAt(0)) //while player hits b, then enter (to bet)
+            {           
+                //player hit b, draw a card, print decks, calulate score
+                
+                
+                
+                hitCountPlayer++;
+                drawCard(deck[4 + (hitCountPlayer)],playerCards);
+                blackJackPrintDecks(dealerCards,playerCards);
+                if(deck[4 + (hitCountPlayer)].value==11)//if next draw is an ace
+                {
+                    playerAces++;
+                }
+                //update playerScore for new Card value
+                playerScore += (deck[(4 + hitCountPlayer)].value); //for first drawn card, draw 5 and add it, then 6, etc
+                System.out.print("\nPlayer Score: " + playerScore);
+            
+                tempAces = playerAces;
+                while(tempAces>0) //print another score for each ace
+                {
+                    temp = playerScore - 10; //For each ace player has, show a score 10 lower
+                    System.out.print(" / " + temp);
+                    tempAces--;            
+                }
+                
+                if((playerScore-(10*playerAces))>21) //check if lowest possible score is over 21
+                {
+                    
+                    blackJackPlayerBust();
+                }
+                System.out.println("");   
+            }
+            if('s'== sc.next().charAt(0))
+            {
+                System.out.println("STAYED");
+            }
+            
+
+            
+            
+            
+            
+            break;
+        }
+    }
+    public static void blackJackPlayerBust()
+    {
+        System.out.println("\nYou Busted, Game Over");
+        System.exit(0);
+    }
+    public static void blackJackBlackJack()
+    {
+        System.out.println("Blackjack! Payout is 3:2");
+        System.exit(0);
+    }
+    public static void blackJackPrintDecks(String[] dealer, String[] player)
+    {
+        System.out.println("******Dealer's Cards******");
+        printCards(dealer);
+        System.out.println("\n\n********Your Cards********");
+        printCards(player);
+        
     }
     public static card[] shuffler(card[] deck)
     {
